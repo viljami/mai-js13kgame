8cfa14df856fc33fe7d49379052beec6eb51d602
Viljami Peltola <viljami.peltola@iki.fi>
2024-08-27 18:54:18 +0300

    Fix buttons left down

--- src/components/input.ts
+++ src/components/input.ts
@@ -19,6 +19,9 @@ export class InputManager {
         const { body } = document;
         body.addEventListener("pointerdown", this.onPointerDown);
         body.addEventListener("pointerup", this.onPointerUp);
+        body.addEventListener("pointerleave", this.onPointerUp);
+        // body.addEventListener("pointerout", this.onPointerUp);
+        body.addEventListener("pointercancel", this.onPointerUp);
     }
 
     disable() {
@@ -26,13 +29,16 @@ export class InputManager {
         const { body } = document;
         body.removeEventListener("pointerdown", this.onPointerDown);
         body.removeEventListener("pointerup", this.onPointerUp);
+        body.removeEventListener("pointerleave", this.onPointerUp);
+        // body.removeEventListener("pointerout", this.onPointerUp);
+        body.removeEventListener("pointercancel", this.onPointerUp);
     }
 
     onPointerDown = ({ x, y, pointerId, timeStamp }: PointerEvent) => {
         this.inputs.push(new Input(pointerId, Vec2.new(x, y), timeStamp));
     }
 
-    onPointerUp = ({ x, y, pointerId, timeStamp }: PointerEvent) => {
+    onPointerUp = ({ pointerId }: PointerEvent) => {
         const index = this.inputs.findIndex(({ id }) => id == pointerId);
 
         if (index != -1) {
