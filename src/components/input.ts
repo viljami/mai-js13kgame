07e889ecff392270c960c671da0f309bb998f222
Viljami Peltola <viljami.peltola@iki.fi>
2024-09-02 16:00:32 +0300

    Improve input handling for keyboard

--- src/components/input.ts
+++ src/components/input.ts
@@ -12,30 +12,78 @@ export class Input {
     }
 }
 
+const KEYBOARD_EVENT_ID = 99999999;
+
 export class InputManager {
     inputs: Input[] = [];
 
     activate() {
+        this.inputs.length = 0;
         const { body } = document;
+        body.addEventListener("keydown", this.onKeyDown);
         body.addEventListener("pointerdown", this.onPointerDown);
+        body.addEventListener("pointermove", this.onPointerUp);
         body.addEventListener("pointerup", this.onPointerUp);
         body.addEventListener("pointerleave", this.onPointerUp);
-        // body.addEventListener("pointerout", this.onPointerUp);
+        body.addEventListener("pointerout", this.onPointerUp);
         body.addEventListener("pointercancel", this.onPointerUp);
     }
 
     disable() {
-        this.inputs.length = 0;
         const { body } = document;
+        body.addEventListener("keyup", this.onKeyUp);
         body.removeEventListener("pointerdown", this.onPointerDown);
+        body.removeEventListener("pointermove", this.onPointerUp);
         body.removeEventListener("pointerup", this.onPointerUp);
         body.removeEventListener("pointerleave", this.onPointerUp);
-        // body.removeEventListener("pointerout", this.onPointerUp);
+        body.removeEventListener("pointerout", this.onPointerUp);
         body.removeEventListener("pointercancel", this.onPointerUp);
+        this.inputs.length = 0;
     }
 
-    onPointerDown = ({ x, y, pointerId, timeStamp }: PointerEvent) => {
+    onKeyDown = ({ key }: KeyboardEvent) => {
+        switch (key) {
+            case "ArrowLeft":
+                this.inputs[0] = new Input(KEYBOARD_EVENT_ID, Vec2.new(0, 0), 0);
+                break;
+            case "ArrowRight":
+                this.inputs[0] = new Input(KEYBOARD_EVENT_ID + 1, Vec2.new(window.innerWidth, 0), 0);
+                break;
+            case "ArrowDown":
+                this.inputs[0] = new Input(KEYBOARD_EVENT_ID + 2, Vec2.new(window.innerWidth / 2, 0), 0);
+                break;
+            default:
+                break;
+        }
+    }
+
+    onKeyUp = ({ key }: KeyboardEvent) => {
+        let index = -1;
+
+        switch (key) {
+            case "ArrowLeft":
+                index = this.inputs.findIndex(({ id }) => id == KEYBOARD_EVENT_ID);
+                break;
+            case "ArrowRight":
+                index = this.inputs.findIndex(({ id }) => id == KEYBOARD_EVENT_ID + 1);
+                break;
+            case "ArrowDown":
+                index = this.inputs.findIndex(({ id }) => id == KEYBOARD_EVENT_ID + 2);
+                break;
+            default:
+                break;
+        }
+
+        if (index != -1) {
+            this.inputs.splice(index, 1);
+        }
+    }
+
+    onPointerDown = (e: PointerEvent) => {
+        e.preventDefault();
+        const { x, y, pointerId, timeStamp } = e;
         this.inputs.push(new Input(pointerId, Vec2.new(x, y), timeStamp));
+        return false;
     }
 
     onPointerUp = ({ pointerId }: PointerEvent) => {
